ad07cf94c3a787f634b23871814519a5a8a2f877
Harsh raj <harshraj9162192367@gmail.com>
2026-02-17 17:27:33 +0530

    feat:modified:src/index.css, modified:src/pages/Product.jsx

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import {createContext} from 'react';
 import {products} from '../assets/assets'
 import { useState } from 'react';
+import { toast } from 'react-toastify'
 
 export const ShopContext = createContext();
 
@@ -10,7 +11,51 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
-     
+    const [cartItems, setCartItems] = useState({});
+
+
+    const addToCart = async (itemId, size) => {
+        if (!size) {
+            toast.error('Please select a Product Size');
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            } else {
+                cartData[itemId][size] = 1;
+            }
+        } else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const items in cartItems) {
+            for (const size in cartItems[items]) {
+                try {
+                    if (cartItems[items][size] > 0) {
+                        totalCount += cartItems[items][size];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
+
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+        setCartItems(cartData);
+    }
+
     const value = {
         products,
         currency,
@@ -18,7 +63,11 @@ const ShopContextProvider = (props) => {
         search,
         setSearch,
         showSearch,
-        setShowSearch
+        setShowSearch,
+        cartItems,
+        addToCart,
+        getCartCount,
+        updateQuantity,
     }
 
     return (
--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -2,10 +2,11 @@ import React,{ useContext, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import Relatedproducts from '../components/Relatedproducts';
 
 const Product = () => {
   const {productId} = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
@@ -67,8 +68,28 @@ const Product = () => {
                 ))}
             </div>
           </div>
+          <button onClick={()=>(addToCart(productData._id,size))} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5' />
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Original Product.</p>
+            <p>Cash on delievery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
       </div>
+      {/* ----------Description & Review Section------- */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p> An e-comerce website is an online platform where businesses sell products or services directly to customers over the internet.</p>
+          <p>E-commerce websites typically feature product listings, shopping carts, secure payment processing, and user account management.</p>
+        </div>
+      </div>
+      {/* --------Display realted products---------- */}
+      <Relatedproducts category={productData.category} subCategory={productData.subCategory} />
     </div>
   ) : <div className='opacity-0'></div>
 }
