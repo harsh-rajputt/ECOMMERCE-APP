52dac388730394a7e641c3611ec924776d937542
Harsh raj <harshraj9162192367@gmail.com>
2026-02-21 12:58:00 +0530

    feat: setup server and root gitignore

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+// App config
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middlewares
+
+app.use(express.json());
+app.use(cors());
+
+// API Endpoints
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Listen
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
